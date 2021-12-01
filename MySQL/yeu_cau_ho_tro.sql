d63f73202ace6012a3136af3217c209852f0ad4f
Nguyễn Hồng Dân <70143744+dannhh@users.noreply.github.com>
2021-12-01 20:26:29 +0700

    yeu cau ho tro

--- MySQL/yeu_cau_ho_tro.sql
+++ MySQL/yeu_cau_ho_tro.sql
@@ -4,19 +4,26 @@ SELECT YEU_CAU_HO_TRO.maYC, loaiYC, noidungYC, trangthaiYC
 FROM YEU_CAU_HO_TRO, GUI_YEU_CAU
 WHERE GUI_YEU_CAU.maVD = 12222221 AND YEU_CAU_HO_TRO.maYC = GUI_YEU_CAU.maYC
 ORDER BY GUI_YEU_CAU.thoigiangui DESC;
+
+
+
 -- hiển thị danh sách các yêu cầu do một người dùng (có thể là nhân viên hoặc chủ) có mã người dùng là 12300001 theo thứ tự thời gian gửi.
-SELECT YEU_CAU_HO_TRO.maYC, loaiYC, noidungYC, trangthaiYC
+SELECT YEU_CAU_HO_TRO.maYC, loaiYC, noidungYC, trangthaiYC, thoigiangui
 FROM YEU_CAU_HO_TRO, GUI_YEU_CAU
 WHERE GUI_YEU_CAU.maND = 12300001 AND YEU_CAU_HO_TRO.maYC = GUI_YEU_CAU.maYC
 ORDER BY GUI_YEU_CAU.thoigiangui DESC;
--- hiển thị các đơn hàng có ít nhất một đơn hàng đã được xử lý theo thứ tự giảm dần số lượng yêu cầu.
+
+
+-- hiển thị các đơn hàng có ít nhất một yêu cầu đã được xử lý theo thứ tự giảm dần số lượng yêu cầu.
 SELECT maVD, COUNT(*) AS soyeucau
 FROM YEU_CAU_HO_TRO, GUI_YEU_CAU
 WHERE YEU_CAU_HO_TRO.trangthaiYC = 2 AND YEU_CAU_HO_TRO.maYC = GUI_YEU_CAU.maYC
 GROUP BY maVD
 HAVING COUNT(*) > 0
 ORDER BY COUNT(*) DESC;
--- hiển thị danh sách người dùng có ít nhất một đơn hàng đang được xử lý theo thứ tự giảm dần số lượng yêu cầu.
+
+
+-- hiển thị danh sách người dùng có ít nhất một yêu cầu đang được xử lý theo thứ tự giảm dần số lượng yêu cầu.
 SELECT maND, COUNT(*) AS soyeucau
 FROM YEU_CAU_HO_TRO, GUI_YEU_CAU
 WHERE YEU_CAU_HO_TRO.trangthaiYC = 1 AND YEU_CAU_HO_TRO.maYC = GUI_YEU_CAU.maYC
@@ -34,7 +41,7 @@ CREATE PROCEDURE traCuuYeuCau(
 )
 BEGIN
 	IF (ma_van_don < 10000000 OR ma_van_don > 99999999) THEN
-		SELECT CONCAT('Mã vận đơn của bạn ', CAST(ma_van_don AS CHAR), 'nằm ngoài miền giá trị!') AS 'Error';
+		SELECT CONCAT('Mã vận đơn của bạn ', CAST(ma_van_don AS CHAR), ' nằm ngoài miền giá trị!') AS 'Error';
 	ELSEIF (EXISTS(SELECT * FROM DON_HANG WHERE maVD = ma_van_don) = 0) THEN
 		SELECT CONCAT('Mã vận đơn ', CAST(ma_van_don AS CHAR), ' không tồn tại!') AS 'Error';
 	ELSE
@@ -47,6 +54,7 @@ END //
 DELIMITER ;
 
 CALL traCuuYeuCau(12222221);
+
 -- thủ tục thêm vào bảng GUI_YEU_CAU
 DROP PROCEDURE IF EXISTS themYeuCau;
 DELIMITER //
@@ -75,8 +83,8 @@ END //
 DELIMITER ;
 
 INSERT INTO YEU_CAU_HO_TRO(maYC, loaiYC, noidungYC)
-VALUES (99000007, 1, 'Test fck tired!');
-CALL themYeuCau(23400001, 99000007, 21111111);
+VALUES (99000008, 1, 'Yêu cầu hỗ trợ 8!');
+CALL themYeuCau(23400001, 99000008, 21111111);
 
 -- yêu cầu số 3
 -- trigger xoá yêu cầu hỗ trợ
@@ -91,11 +99,14 @@ END//
 
 DELIMITER ;
 
-DELETE FROM GUI_YEU_CAU WHERE maYC = 99000007;
+DELETE FROM GUI_YEU_CAU WHERE maYC = 99000008;
+
+
+
 -- trigger xoá yêu cầu hỗ trợ khi xoá đơn hàng
 DELIMITER //
 
-CREATE TRIGGER xoaYCHT
+CREATE TRIGGER xoaYCHT_don_hang
     BEFORE DELETE
     ON DON_HANG FOR EACH ROW
 BEGIN
@@ -103,6 +114,9 @@ BEGIN
 END//  
 
 DELIMITER ;
+
+DELETE FROM DON_HANG WHERE maVD = 22222221;
+
 -- trigger cập nhật thời gian khi chỉnh sửa
 DELIMITER //
 
@@ -116,6 +130,8 @@ BEGIN
 END//  
 
 DELIMITER ;
+
+UPDATE yeu_cau_ho_tro SET noidungYC="Yêu cầu mới" WHERE maYC = 99000005
 
 -- yêu cầu số 4
 -- hàm trả về số yêu cầu hỗ trợ do một người dùng (có thể là chủ hoặc nhân viên) gửi tới
